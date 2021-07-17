ba1d04533f36219a4c20b7dbbd326de2bdb666f8
jekin000 <kira_r163@163.com>
2021-07-17 14:25:26 +0000

    Add simple log

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"log"
-	"os"
+	"jekin000/mygoproject/pkg/gologger"
+	"fmt"
 )
 
+
 func main() {
-	fileName := "/var/log/mydebug.log"
-	logFile, err := os.Create(fileName)
-	defer logFile.Close()
-	if err != nil {
-		log.Fatalln("Open file failed")
-	}
-	debugLog := log.New(logFile, "[Debug]", log.Llongfile)
-	debugLog.Println("A debug msg here")
-	debugLog.SetPrefix("[Info]")
-	debugLog.Println("A info msg here")
-	debugLog.SetFlags(debugLog.Flags() | log.LstdFlags)
-	debugLog.Println("A different prefix")
+	logger := gologger.InitLogger()
+	logger.Debug(fmt.Sprintf("it is debug,%d",1))
+	logger.Info(fmt.Sprintf ("it is info ,%d",2))
 }
--- pkg/gologger/gologger.go
+++ pkg/gologger/gologger.go
@@ -1,7 +1,47 @@
 package gologger
 
-import "fmt"
+import (
+	//"fmt"
+	"log"
+	//"io"
+	"os"
+	//"reflect"
+)
 
-func Hello(){
-	fmt.Println("logger Hellor")
+type Logger struct{
+	ioWriter *os.File
+	debugLogger *log.Logger
+	infoLogger *log.Logger
+}
+
+func InitLogger() *Logger{
+	fileName := "/var/log/mydebug.log"
+	logFile, err := os.OpenFile(fileName,os.O_APPEND|os.O_WRONLY,os.ModeAppend)
+	if err != nil {
+		log.Fatalln("Open file failed,%s",err)
+	}
+	debugLogger := log.New(logFile, "[Debug]",log.Llongfile|log.LstdFlags)
+	infoLogger  := log.New(logFile, "[Info ]",log.Llongfile|log.LstdFlags)
+
+	//fmt.Println(reflect.TypeOf(infoLogger))
+
+	logger := &Logger{
+		ioWriter : logFile,
+		debugLogger : debugLogger,
+		infoLogger  : infoLogger,
+	}
+
+	return logger
+}
+
+func (l *Logger) Close(){
+	l.ioWriter.Close()
+}
+
+func (l *Logger) Debug (str string){
+	l.debugLogger.Println(str)
+}
+
+func (l *Logger) Info (str string){
+	l.infoLogger.Println(str)
 }
